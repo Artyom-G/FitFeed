7c3a7b7ff53c697d59de6ab7646da97aa9944d16
Artyom Gabtraupov <artyomgv05@gmail.com>
2024-05-09 17:58:51 -0400

    added a bunch of shi

--- components/googleOAuth.jsx
+++ components/googleOAuth.jsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import * as WebBrowser from "expo-web-browser";
 import * as Google from 'expo-auth-session/providers/google';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const clientIDs = require('../clientIDs.json');
+const globalStyles = require("../globalStyles.json");
 
 WebBrowser.maybeCompleteAuthSession();
 
-function GoogleOAuth({ navigation }) {
+function GoogleOAuth() {
     const [userInfo, setUserInfo] = React.useState(null);
     const [request, response, promptAsync] = Google.useAuthRequest({
         androidClientId: clientIDs.android,
         iosClientId: clientIDs.ios,
         webClientId: clientIDs.web
     });
+    const [signedIn, setSignedIn] = React.useState(false);
 
     React.useEffect(() => {
         handleSignInWithGoogle();
@@ -32,6 +34,7 @@ function GoogleOAuth({ navigation }) {
             //await getUserInfo();
         } else {
             setUserInfo(JSON.parse(user));
+            setSignedIn(true);
             //console.log("test1");
             //console.log(JSON.stringify(userInfo));
         }
@@ -51,6 +54,7 @@ function GoogleOAuth({ navigation }) {
             //console.log(user);
             await AsyncStorage.setItem("@user", JSON.stringify(user));
             setUserInfo(user);
+            setSignedIn(true);
         } catch (error) {
             console.log("error!");
             console.log(error);
@@ -60,15 +64,55 @@ function GoogleOAuth({ navigation }) {
     const DeleteUserData = () => {
         AsyncStorage.removeItem("@user");
         setUserInfo(null);
+        setSignedIn(false);
     }
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Button title="Sign in with Google" onPress={() => promptAsync()} />
-            <Button title="Log out" onPress={() => DeleteUserData()} />
-            <Text>{JSON.stringify(userInfo, null, 2)}</Text>
-        </View>
+        signedIn ?
+            <View style={styles.container}>
+                <Image source={{ uri: userInfo.picture }} style={styles.image}></Image>
+                <Text style={styles.text}>{userInfo.email}</Text>
+                <TouchableOpacity style={styles.redButton} onPress={() => DeleteUserData()}>
+                    <Text style={styles.buttonText}>Log out</Text>
+                </TouchableOpacity>
+            </View>
+            :
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.redButton} onPress={() => promptAsync()}>
+                    <Text style={styles.buttonText}>Sign in with Google</Text>
+                </TouchableOpacity>
+            </View>
     );
 }
 
 export default GoogleOAuth;
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 20
+    },
+    infoWrapper: {
+
+    },
+    image: {
+        width: 50,
+        height: 50,
+        borderRadius: 100
+    },
+    text: {
+        color: globalStyles.activePrimaryColor,
+        fontWeight: 'bold',
+        paddingLeft: 10
+    },
+    redButton: {
+        backgroundColor: globalStyles.activePrimaryColor,
+        padding: 10,            
+        borderRadius: 5
+    },
+    buttonText: {
+        color: 'white',        
+        fontWeight: 'bold'      
+    }
+});
--- screens/profileScreen.jsx
+++ screens/profileScreen.jsx
@@ -6,7 +6,6 @@ function ProfileScreen({ navigation }) {
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Profile Screen</Text>
             <GoogleOAuth />
         </View>
     );
