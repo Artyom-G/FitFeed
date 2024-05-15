c99ef40546f9adc46947376e4c0a5129218ad978
Artyom Gabtraupov <artyomgv05@gmail.com>
2024-05-15 18:31:06 -0400

    added firebase auth

---
+++ components/profileFirebaseHeader.jsx
@@ -0,0 +1,182 @@
+import * as React from 'react';
+import { Button, View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import * as WebBrowser from "expo-web-browser";
+import * as Google from 'expo-auth-session/providers/google';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+
+const clientIDs = require('../private/clientIDs.json');
+const globalStyles = require("../globalStyles.json");
+
+GoogleSignin.configure({
+    androidClientId: clientIDs.android,
+    iosClientId: clientIDs.ios,
+    webClientId: clientIDs.web
+});
+
+WebBrowser.maybeCompleteAuthSession();
+
+function ProfileFirebaseHeader() {
+    async function onGoogleButtonPress() {
+        // Check if your device supports Google Play
+        await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+        // Get the users ID token
+        const { idToken } = await GoogleSignin.signIn();
+      
+        // Create a Google credential with the token
+        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      
+        // Sign-in the user with the credential
+        return auth().signInWithCredential(googleCredential);
+    }
+
+    const [userInfo, setUserInfo] = React.useState(null);
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        androidClientId: clientIDs.android,
+        iosClientId: clientIDs.ios,
+        webClientId: clientIDs.web
+    });
+    const [signedIn, setSignedIn] = React.useState(false);
+
+    React.useEffect(() => {
+        handleSignInWithGoogle();
+    }, [response]);
+
+    async function handleSignInWithGoogle() {
+        //console.log("t3");
+        const user = await AsyncStorage.getItem("@fitfeedUser");
+        if (!user) {
+            ///console.log("test2");
+            if (response?.type === "success") {
+                await getUserInfo(response.authentication.accessToken);
+                console.log("Success!!");
+            }
+            //await getUserInfo();
+        } else {
+            setUserInfo(JSON.parse(user));
+            setSignedIn(true);
+            //console.log("test1");
+            //console.log(JSON.stringify(userInfo));
+        }
+    }
+
+    const getUserInfo = async (token) => {
+        if (!token) return;
+        try {
+            const response = await fetch(
+                "https://www.googleapis.com/userinfo/v2/me",
+                {
+                    headers: { Authorization: `Bearer ${token}` },
+                }
+            );
+            //console.log(response);
+            const user = await response.json();
+            //console.log(user);
+            await AsyncStorage.setItem("@fitfeedUser", JSON.stringify(user));
+            setUserInfo(user);
+            setSignedIn(true);
+        } catch (error) {
+            console.log("error!");
+            console.log(error);
+        }
+    }
+
+    const DeleteUserData = () => {
+        AsyncStorage.removeItem("@fitfeedUser");
+        setUserInfo(null);
+        setSignedIn(false);
+    }
+
+    return (
+        signedIn ?
+            <View style={styles.container}>
+                <View style={styles.bioProfileWrapper}>
+                    <View style={styles.profile}>
+                        <Image source={{ uri: userInfo.picture }} style={styles.image}></Image>
+                        <View style={styles.profileText}>
+                            <Text style={styles.text}>{userInfo.name}</Text>
+                            <Text>Elite Powerlifter</Text>
+                            <View style={styles.profileMedals}>
+                                <Icon name={'star'} size={globalStyles.profileMedalIconSize} color={globalStyles.activePrimaryColor} />
+                                <Icon name={'star'} size={globalStyles.profileMedalIconSize} color={globalStyles.activePrimaryColor} />
+                                <Icon name={'star'} size={globalStyles.profileMedalIconSize} color={globalStyles.activePrimaryColor} />
+                                <Text>+5 more..</Text>
+                            </View>
+                        </View>
+                    </View>
+                    <Text>This Bio is very long lol</Text>
+                    <Text>Bogus Bogus Bogus</Text>
+                    <Text>Bingus Bingus Bingus</Text>
+                    <Text>Pump Homie is Elite</Text>
+                    <TouchableOpacity style={styles.singInButton} onPress={() => DeleteUserData()}>
+                        <Text style={styles.buttonText}>Log out</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            :
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.singInButton} onPress={() => onGoogleButtonPress()}>
+                    <Text style={styles.buttonText}>Sign in with Google</Text>
+                </TouchableOpacity>
+            </View>
+    );
+}
+
+export default ProfileFirebaseHeader;
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 20,
+        width: '100%',
+        paddingBottom: 40
+    },
+    bioProfileWrapper:{
+        width: '90%',
+        justifyContent: 'left',
+        flexDirection: 'column',
+        gap: 10
+    },
+    profile:{
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 40
+    },
+    profileText:{
+        flexDirection: 'column',
+        gap: 10
+    },
+    profileMedals:{
+        display: 'flex',
+        flexDirection: 'row',
+        alignContent: 'center'
+    },
+
+    infoWrapper: {
+
+    },
+    image: {
+        width: globalStyles.profileLogoSize,
+        height: globalStyles.profileLogoSize,
+        borderRadius: 100
+    },
+    text: {
+        color: globalStyles.activePrimaryColor,
+        fontWeight: 'bold',
+        fontSize: 22,
+    },
+    singInButton: {
+        backgroundColor: globalStyles.activePrimaryColor,
+        padding: 10,            
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',        
+        fontWeight: 'bold',
+        textAlign: 'right'    
+    }
+});
