67dc61f5b6812140c1eea8ac5b09161598889759
Artyom Gabtraupov <artyomgv05@gmail.com>
2024-05-16 16:14:30 -0400

    firebase database test script + added the android folder too

---
+++ components/firebaseTest.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Platform, KeyboardAvoidingView, Button } from 'react-native';
+import { firebase } from '@react-native-firebase/database';
+import database from '@react-native-firebase/database';
+
+const globalStyles = require('../globalStyles.json');
+
+const FirebaseTest = () => {
+
+    const writeToDatabase = async () => {
+        try{
+            await database().ref('/users/123').set({
+                name: 'user name',
+                age: 31,
+            });
+            console.log('success');
+        }
+        catch(error){
+            console.log('error: ', error);
+        }
+    }
+
+    return (  
+        <View>
+            <Button title="write to database" onPress={writeToDatabase} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        width: '90%',
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: 'space-between'
+    }
+
+});
+
+export default FirebaseTest;
