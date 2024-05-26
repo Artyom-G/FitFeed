f06e0684350afdf8155d8619cb86783e4f7bb960
Artyom Gabtraupov <artyomgv05@gmail.com>
2024-05-26 18:46:54 -0400

    using eas secrets now

--- app.config.js
+++ app.config.js
@@ -1,8 +1,50 @@
-export default () => ({
-    ios: {
+export default ({ config }) => ({
+    ...config,
+    expo: {
+      ...config.expo,
+      name: "fitfeed",
+      slug: "fitfeed",
+      version: "1.0.0",
+      orientation: "portrait",
+      icon: "./assets/icon.png",
+      userInterfaceStyle: "light",
+      splash: {
+        image: "./assets/splash.png",
+        resizeMode: "contain",
+        backgroundColor: "#ffffff"
+      },
+      ios: {
+        supportsTablet: true,
         googleServicesFile: process.env.GOOGLE_SERVICE_INFO_PLIST,
-    },
-    android: {
+      },
+      android: {
+        adaptiveIcon: {
+          foregroundImage: "./assets/adaptive-icon.png",
+          backgroundColor: "#ffffff"
+        },
+        package: "com.citricmelon.fitfeed",
+        permissions: [
+          "CAMERA"
+        ],
         googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
-    },
-});
+      },
+      web: {
+        favicon: "./assets/favicon.png"
+      },
+      extra: {
+        eas: {
+          projectId: "7d83e923-f275-42fe-b81b-741c113cb534"
+        }
+      },
+      scheme: "com.citricmelon.fitfeed",
+      plugins: [
+        [
+          "expo-camera",
+          {
+            cameraPermission: "Allow $(PRODUCT_NAME) to access your camera"
+          }
+        ]
+      ]
+    }
+  });
+  
