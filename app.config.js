1b331a76b3170a87db3450d3288cb9ec77c320d2
Artyom Gabtraupov <artyomgv05@gmail.com>
2024-05-26 20:54:37 -0400

    using eas secrets now

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default ({ config }) => ({
       ...config.expo,
       name: "fitfeed",
       slug: "fitfeed",
-      version: "1.0.0",
+      version: "1.0.1",
       orientation: "portrait",
       icon: "./assets/icon.png",
       userInterfaceStyle: "light",
